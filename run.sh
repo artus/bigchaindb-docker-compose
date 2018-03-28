4f6a1ed9e63b18b0b0dd3bdcd66228ccc84341ba
artusvranken <artusvranken@gmail.com>
2018-03-28 16:20:05 +0200

    Added a script that performs the one-time configuration.

---
+++ run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Artus Vranken
+
+if [ -f $HOME/bigchaindb_docker/.bigchaindb ]; then
+	echo "Configuration already exists, skipping..."
+else
+	echo "Configuring bigchaindb-mongodb..."
+	docker run -it --rm -v $HOME/bigchaindb_docker:/data --env BIGCHAINDB_DATABASE_HOST=172.17.0.1 artusvranken/alpine-bigchaindb -y configure mongodb
+fi
+
+echo "Done, starting up docker-compose..."
+
+docker-compose up -d
